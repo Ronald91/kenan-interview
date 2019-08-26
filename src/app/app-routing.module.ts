d6efd4dde64184b7d8fc4b886623a6941800b67e
Ronald Williams <rwilliams0291@gmail.com>
2019-08-26 18:13:56 -0400

    added lazy loading and envs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FunWithFormsComponent } from './fun-with-forms/fun-with-forms.component';
-
+import { LazyComponent } from './lazy/lazy.component';
 
 const routes: Routes = [
   {
-path: 'forms',
-component: FunWithFormsComponent
-}];
+    path: 'forms',
+    component: FunWithFormsComponent
+  },
+  {
+    path: 'customers',
+    loadChildren: () =>
+      import('./lazy/lazy.module').then(mod => mod.LazyModule)
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule],
-
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
